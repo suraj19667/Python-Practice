995727d0143348250dfa59d1df448c2be32b90b4
Suraj <sy083609@gmail.com>
2025-11-04 15:15:02 +0530

    updated commit

--- number_count.py
+++ number_count.py
@@ -3,4 +3,4 @@ from math import *
 num = int(input("Enter a number: "))
 def number_count(num):
     return int (log10(num))+1
-print("Number of digits in", num, "is", number_count(num))
+print("Number of  Digits : ", num, "is", number_count(num))
