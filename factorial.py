573c4daa3d4faf2a5b0f9ad45c33570ce5e5d547
Suraj <sy083609@gmail.com>
2025-11-05 16:54:22 +0530

    updated commit

--- factorial.py
+++ factorial.py
@@ -9,23 +9,23 @@
 
 # -------------------- Batter Solution second method -----------------------
 
-# num=int(input("Enter the number:"))
-# result=[]
-# for i in range(1,num//2+1):
-#     if num%i==0:
-#         result.append(i)
-# result.append(num)
-# print(result)
+num=int(input("Enter the number:"))
+result=[]
+for i in range(1,num//2+1):
+    if num%i==0:
+        result.append(i)
+result.append(num)
+print(result)
 
 # -----------------  Optimal Solution third method.  -----------------------------
 
-from math import sqrt
-num=int(input("Enter the number:"))
-result=[]
-for i in range(1,int(sqrt(num)+1)):
-    if num%i==0:
-        result.append(i)
-        if num//i !=i:
-            result.append(num//i)
-result.sort()
-print(result)
+# from math import sqrt
+# num=int(input("Enter the number:"))
+# result=[]
+# for i in range(1,int(sqrt(num)+1)):
+#     if num%i==0:
+#         result.append(i)
+#         if num//i !=i:
+#             result.append(num//i)
+# result.sort()
+# print(result)
