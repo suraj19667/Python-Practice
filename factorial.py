6421860e6de26c8c5c98cc76b12864ba0a6ec3f0
Suraj <sy083609@gmail.com>
2025-11-08 11:00:57 +0530

    updated commit

--- factorial.py
+++ factorial.py
@@ -1,11 +1,11 @@
-# num=int(input("Enter the number:"))
-# result=[]
+num=int(input("Enter the number:"))
+result=[]
 
-# for i in range(1,num+1):
-#     if num%i==0:
-#         result.append(i)
+for i in range(1,num+1):
+    if num%i==0:
+        result.append(i)
 
-# print(result)
+print(result)
 
 # -------------------- Batter Solution second method -----------------------
 
