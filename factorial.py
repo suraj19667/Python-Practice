252984d15367bc33beee239ac020753089c5e139
Suraj <sy083609@gmail.com>
2025-11-09 16:39:56 +0530

    updated commit

--- factorial.py
+++ factorial.py
@@ -19,13 +19,13 @@ print(result)
 
 # -----------------  Optimal Solution third method.  -----------------------------
 
-# from math import sqrt
-# num=int(input("Enter the number:"))
-# result=[]
-# for i in range(1,int(sqrt(num)+1)):
-#     if num%i==0:
-#         result.append(i)
-#         if num//i !=i:
-#             result.append(num//i)
-# result.sort()
-# print(result)
+from math import sqrt
+num=int(input("Enter the number:"))
+result=[]
+for i in range(1,int(sqrt(num)+1)):
+    if num%i==0:
+        result.append(i)
+        if num//i !=i:
+            result.append(num//i)
+result.sort()
+print(result)
