ccc3836758a336b1cb4580bb04fdc16a4d20f620
Suraj <sy083609@gmail.com>
2025-12-27 13:58:14 +0530

    updated commit

---
+++ list_any_all.py
@@ -0,0 +1,44 @@
+# l=[3,2,1,0,3,0,0]
+# l1=[0,0,0]
+# print(any(l))
+# print(all(l))
+# print(any(l1))
+# print(all(l1))
+
+
+# empty_list = []
+# int=[]
+# print(any(empty_list))
+# print(all(empty_list))
+# i=0
+# print(bool(i))
+
+# i=int()
+# print(i)
+
+# ------ Str -------
+# s=' ' 
+# print(bool(s))
+# print()
+# ---------- list --------
+
+# l=[]
+# l2=list()
+# l1=[3,4,5,5]
+# print(bool(l))
+# print(bool(l2))
+# print(bool(l1))
+
+# # --------tuple --------
+
+# t=()
+# tuple()
+# t1=(4,5,6)
+# print(bool(t))
+# print(bool(tuple))
+# print(bool(t1))
+
+x=0
+y='a'
+print(any((x,y)))     #or
+print(all((x,y)))     #and
